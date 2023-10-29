27690167bed9feff415a3d6679e1e590e320a3fa
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-29 14:26:15 -0700

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,19 +1,14 @@
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
 
 class Question:
     def __init__(self, question, options, correct_option):
         self.question = question
         self.options = options
         self.correct_option = correct_option
-
-    def display_question(self):
-        print(self.question)
-        for idx, option in enumerate(self.options, start=1):
-            print(f"{idx}. {option}")
 
     def check_answer(self, user_choice):
         return user_choice == self.correct_option
@@ -26,31 +21,47 @@ class QuizApp(App):
             Question("Which is the largest mammal?", ["Elephant", "Blue Whale", "Giraffe"], 2)
         ]
         self.score = 0
+        self.current_question = 0
 
-        layout = BoxLayout(orientation='vertical')
+        layout = GridLayout(cols=1)
+
         self.question_label = Label(text="Simple Quiz Game", font_size=30)
         layout.add_widget(self.question_label)
 
-        for idx, question in enumerate(self.questions, start=1):
-            question_label = Label(text=f"Question {idx}: {question.question}")
-            layout.add_widget(question_label)
-            for i, option in enumerate(question.options, start=1):
-                btn = Button(text=option)
-                btn.bind(on_press=self.check_answer_wrapper(idx, i))
-                layout.add_widget(btn)
+        self.options_layout = GridLayout(cols=1)
 
+        layout.add_widget(self.options_layout)
+
+        self.show_question()
         return layout
 
-    def check_answer_wrapper(self, question_idx, user_choice):
-        def inner(instance):
-            question = self.questions[question_idx - 1]
-            if question.check_answer(user_choice):
-                print("Correct!")
-                self.score += 1
-            else:
-                print("Incorrect.")
+    def show_question(self):
+        self.options_layout.clear_widgets()
+        question = self.questions[self.current_question]
 
-        return inner
+        question_label = Label(text=f"Question {self.current_question + 1}: {question.question}", font_size=20)
+        self.options_layout.add_widget(question_label)
+
+        for i, option in enumerate(question.options, start=1):
+            btn = Button(text=option, size_hint_y=None, height=40)
+            btn.bind(on_press=lambda instance, i=i: self.check_answer(i))
+            self.options_layout.add_widget(btn)
+
+    def check_answer(self, user_choice):
+        question = self.questions[self.current_question]
+        if question.check_answer(user_choice):
+            self.score += 1
+
+        if self.current_question < len(self.questions) - 1:
+            self.current_question += 1
+            self.show_question()
+        else:
+            self.show_result_popup()
+
+    def show_result_popup(self):
+        content = Label(text=f"Your final score: {self.score}/{len(self.questions)}", font_size=20)
+        popup = Popup(title='Quiz Result', content=content, size_hint=(None, None), size=(400, 400))
+        popup.open()
 
 if __name__ == '__main__':
     QuizApp().run()
