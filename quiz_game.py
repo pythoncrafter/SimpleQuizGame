52d3a285d3282136bdff9515ead583725ecc4b5f
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-29 14:25:01 -0700

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
 class Question:
     def __init__(self, question, options, correct_option):
         self.question = question
@@ -8,28 +14,43 @@ class Question:
         print(self.question)
         for idx, option in enumerate(self.options, start=1):
             print(f"{idx}. {option}")
-        
+
     def check_answer(self, user_choice):
         return user_choice == self.correct_option
 
-if __name__ == "__main__":
-    questions = [
-        Question("What is the capital of France?", ["Paris", "Berlin", "Rome"], 1),
-        Question("Which planet is known as the Red Planet?", ["Mars", "Venus", "Jupiter"], 1),
-        Question("Which is the largest mammal?", ["Elephant", "Blue Whale", "Giraffe"], 2)
-    ]
-    
-    print("Simple Quiz Game")
-    score = 0
+class QuizApp(App):
+    def build(self):
+        self.questions = [
+            Question("What is the capital of France?", ["Paris", "Berlin", "Rome"], 1),
+            Question("Which planet is known as the Red Planet?", ["Mars", "Venus", "Jupiter"], 1),
+            Question("Which is the largest mammal?", ["Elephant", "Blue Whale", "Giraffe"], 2)
+        ]
+        self.score = 0
 
-    for idx, question in enumerate(questions, start=1):
-        print(f"\nQuestion {idx}:")
-        question.display_question()
-        user_choice = int(input("Enter your choice: "))
-        if question.check_answer(user_choice):
-            print("Correct!")
-            score += 1
-        else:
-            print("Incorrect.")
+        layout = BoxLayout(orientation='vertical')
+        self.question_label = Label(text="Simple Quiz Game", font_size=30)
+        layout.add_widget(self.question_label)
 
-    print(f"\nYour final score: {score}/{len(questions)}")
+        for idx, question in enumerate(self.questions, start=1):
+            question_label = Label(text=f"Question {idx}: {question.question}")
+            layout.add_widget(question_label)
+            for i, option in enumerate(question.options, start=1):
+                btn = Button(text=option)
+                btn.bind(on_press=self.check_answer_wrapper(idx, i))
+                layout.add_widget(btn)
+
+        return layout
+
+    def check_answer_wrapper(self, question_idx, user_choice):
+        def inner(instance):
+            question = self.questions[question_idx - 1]
+            if question.check_answer(user_choice):
+                print("Correct!")
+                self.score += 1
+            else:
+                print("Incorrect.")
+
+        return inner
+
+if __name__ == '__main__':
+    QuizApp().run()
